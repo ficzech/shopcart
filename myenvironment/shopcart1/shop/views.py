e10d316cd7c47073da9da8a8bd6c099ec3ed444f
ficzech <ficzech@gmail.com>
2019-12-14 16:40:52 +0100

    styling pages

--- myenvironment/shopcart1/shop/views.py
+++ myenvironment/shopcart1/shop/views.py
@@ -4,19 +4,26 @@ from .models import Category, Product
 
 
 def index(request):
-	text_var = 'This is my first django app web page.'
-	return HttpResponse(text_var)
+    text_var = 'This is my first django app web page.'
+    return HttpResponse(text_var)
 
 
 # Category view
 
 def allProdCat(request, c_slug=None):
-	c_page = None
-	products = None
-	if c_slug is not None:
-		c_page = get_object_or_404(Category, slug=c_slug)
-		products = Product.objects.filter(category=c_page, available=True)
-	else:
-		products = Product.objects.all().filter(available=True)
-	return render(request, 'shop/category.html', {'category': c_page, 'products': products})
+    c_page = None
+    products = None
+    if c_slug is not None:
+        c_page = get_object_or_404(Category, slug=c_slug)
+        products = Product.objects.filter(category=c_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'shop/category.html', {'category': c_page, 'products': products})
 
+
+def ProdCatDetail(request, c_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=c_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'shop/product.html', {'product': product})
